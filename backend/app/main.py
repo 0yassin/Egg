412a3278f2f22d473872bc4ef7caa8b59bfde866
Naitik Sahu <naiitik1526@gmail.com>
2026-09-15 07:27:13 -0300

    setup of routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
 
+from fastapi.responses import HTMLResponse
+app = FastAPI()
 
-def create_app():
-    app = FastAPI()
-    
-    @app.get("/")
-    def home():
-        return ("humm knock knockkk")
-    return app
+posts : list[dict] =[
+    {
+        "id":1,
+        "author":"naitik",
+        "title":"creating an egg",
+        "content":"i use to be a very nerd guy but now i just play games",
+        "date-posted":"15th sept",
+    },
+    {
+        "id":2,
+        "author":"yassin",
+        "title":"creating an egg",
+        "content":"i use to be a very nerd guy but now i just play games",
+        "date-posted":"15th sept",
+    },
+]
 
-
-app = create_app()
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/post", response_class= HTMLResponse, include_in_schema=False)
+def home():
+    return f"<h1>{posts[0]['content']}</h1>"
+  
+@app.get("/api/posts")
+def get_posts():
+    return posts
