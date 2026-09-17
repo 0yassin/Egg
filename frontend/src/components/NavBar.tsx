eadc19603637b2fa2a30ff0b1e1b16a5d182583d
Xitiz Magar <xitizmgr1234567890@gmail.com>
2026-09-17 13:38:14 +0545

    simple navbar is done

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,14 +1,23 @@
 import { NavLink } from "react-router-dom";
 import type { NavLinkRenderProps } from "react-router-dom";
 
-const getNavLinkClass = ({ isActive }: NavLinkRenderProps): string => (isActive ? "font-semibold text-[var(--accent-color)]" : "text-[var(--text-color)]");
+const getNavLinkClass = ({ isActive }: NavLinkRenderProps): string => (isActive ? "font-semibold text-[var(--hightlight-text-color)]" : "text-[var(--text-color)] before:content-[''] before:block before-W-1 before:bg-black");
 
 export function NavBar() {
   return (
-    <nav className="flex items-center justify-between border-b border-[var(--border-color)] px-8 py-4">
-      <NavLink to="/" className={getNavLinkClass}>
-        Home
-      </NavLink>
+    <nav className="flex items-center justify-between border-b border-[var(--border-color)] px-10 py-4 bg-[var(--bg-color)]">
+      <span className="font-semibold text-2xl tracking-tight upper">Egg</span>
+      <div className="flex items-center gap-20 ">
+        <NavLink to="/" className={getNavLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/farm" className={getNavLinkClass}>
+         My Farm
+        </NavLink>
+        <NavLink to="/account" className={getNavLinkClass}>
+          Account
+        </NavLink>
+      </div>
     </nav>
   );
 }
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,4 +1,4 @@
-export function Home(){
+export function Register(){
     return(
         <main>
             main
