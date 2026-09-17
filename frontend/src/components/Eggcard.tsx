be959fe1173d02a0a6e4d60f23446602e21153f6
0yassin <yassinz8275@gmail.com>
2026-09-17 15:01:03 +0100

    added poppins font to eggcard and navbar

--- frontend/src/components/Eggcard.tsx
+++ frontend/src/components/Eggcard.tsx
@@ -10,9 +10,9 @@ export interface EggCardProps {
 
 export default function Eggcard({title, description, isLocked, onClick, unlockDate}: EggCardProps){
     return(
-        <div className="border flex flex-col gap-3 rounded-xl hover:bg-(--border-color)/25 transition-colors border-(--dark-brown) py-3 px-6 max-w-80 w-screen cursor-pointer">
+        <div className="border font-poppins flex flex-col gap-3 rounded-xl hover:bg-(--border-color)/25 transition-colors border-(--dark-brown) py-3 px-6 max-w-80 w-screen cursor-pointer">
             <div className="flex flex-col">
-                <span className="text-[28px] text-(--dark-brown)">{title}</span>
+                <span className="text-[28px] text-(--dark-brown) font-medium">{title}</span>
                 <span className="text-[17.5px] text-(--light-brown)">{description}</span>
             </div>
             <div className="flex justify-between w-full content-center items-center">
