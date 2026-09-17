4777261ec10153ba14c9c465dacf1b3cbcbe0421
0yassin <yassinz8275@gmail.com>
2026-09-17 13:50:25 +0100

    made basic card layout

--- frontend/src/components/Eggcard.tsx
+++ frontend/src/components/Eggcard.tsx
@@ -10,13 +10,13 @@ export interface EggCardProps {
 
 export default function Eggcard({title, description, isLocked, onClick, unlockDate}: EggCardProps){
     return(
-        <div className="border-2 flex flex-col gap-3 rounded-xl border-(--border-color) py-3 px-6 max-w-80 w-screen cursor-pointer">
+        <div className="border flex flex-col gap-3 rounded-xl hover:bg-(--border-color)/25 transition-colors border-(--dark-brown) py-3 px-6 max-w-80 w-screen cursor-pointer">
             <div className="flex flex-col">
                 <span className="text-[28px] text-(--dark-brown)">{title}</span>
-                <span className="text-[17.5px]">{description}</span>
+                <span className="text-[17.5px] text-(--light-brown)">{description}</span>
             </div>
             <div className="flex justify-between w-full content-center items-center">
-                <span className="text-[17.5px] text-center">{unlockDate}</span>
+                <span className="text-[17.5px] text-center text-(--light-brown)">{unlockDate}</span>
                 <span>
                     {isLocked && <img src={lockicon} className='h-8 w-8'/>}
                 </span>
