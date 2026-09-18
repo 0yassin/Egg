e77c484644e05717f40dbfb6aa4ac0f0b1deb647
Xitiz Magar <xitizmgr1234567890@gmail.com>
2026-09-19 00:30:26 +0545

    navbar is finished and starting home page

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,22 +1,32 @@
 import { NavLink } from "react-router-dom";
 import type { NavLinkRenderProps } from "react-router-dom";
+import Button from "./botton";
 
-const getNavLinkClass = ({ isActive }: NavLinkRenderProps): string => (` " relative before:absolute top before:content-[''] before:block before-W-1 before:bg-black "   ${isActive} ? "font-semibold text-[var(--hightlight-text-color)]" : "text-[var(--text-color)]"`);
+const getNavLinkClass = ({ isActive }: NavLinkRenderProps): string =>
+  `relative inline-block py-1 transition-colors before:content-[''] before:absolute before:bottom-0 before:left-1/2 before:h-[2px] before:w-full before:bg-black before:-translate-x-1/2 before:scale-x-0 before:transition-transform before:duration-300 before:ease-out hover:before:scale-x-100 ${
+    isActive ? "font-semibold text-[var(--hightlight-text-color)] " : "text-[var(--text-color)]"
+  }`;
 
 export function NavBar() {
   return (
-    <nav className="font-poppins flex items-center justify-between border-b border-(--border-color) px-10 py-4 bg-(--bg-color)">
-      <span className="font-semibold text-2xl tracking-tight upper">Egg</span>
-      <div className="flex items-center gap-20 ">
+    <nav className="flex items-center justify-between border-b border-[var(--border-color)] px-10 py-4 bg-[var(--bg-color)]">
+      <span className="font-semibold text-2xl tracking-tight upper inline-block ">Egg</span>
+      <div className="flex items-center justify-center gap-20">
         <NavLink to="/" className={getNavLinkClass}>
           Home
         </NavLink>
         <NavLink to="/farm" className={getNavLinkClass}>
-         My Farm
+          My Farm
         </NavLink>
         <NavLink to="/account" className={getNavLinkClass}>
           Account
         </NavLink>
+      </div>
+      <div className="flex items-center justify-center gap-5">
+        <NavLink to="/register">
+        <Button variant="secondary">Start To Plant</Button>
+        </NavLink>
+        {/* <Button variant="secondary">Login</Button> */}
       </div>
     </nav>
   );
