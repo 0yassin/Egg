141effe491fad623ed8ea264446c88d1664d0933
Xitiz Magar <xitizmgr1234567890@gmail.com>
2026-09-20 19:34:17 +0545

    change some typo

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,27 +5,27 @@ export default function Footer(){
                 {/* links */}
                 <div className="text-[22px] font-medium  flex-col flex md:flex-row gap-6 md:gap-12  ">
                     <div className="flex flex-col gap-3">
-                        <a className= "text-(--bg-color)/75 hover:text-(--bg-color) transition-colors" href="/">home</a>
-                        <a className= "text-(--bg-color)/75 hover:text-(--bg-color) transition-colors" href="/farm">farm</a>
-                        <a className= "text-(--bg-color)/75 hover:text-(--bg-color) transition-colors" href="/account">account</a>
+                        <a className= "text-(--bg-color)/75 hover:text-(--bg-color) transition-colors" href="/">Home</a>
+                        <a className= "text-(--bg-color)/75 hover:text-(--bg-color) transition-colors" href="/farm">Farm</a>
+                        <a className= "text-(--bg-color)/75 hover:text-(--bg-color) transition-colors" href="/account">Account</a>
                     </div>
                     <div className="flex flex-col gap-2">
                         <a
                             href="/register"
                             className=" text-white w-full text-center bg-(--accent-blue) py-2 px-6 rounded-lg transition-all cursor-pointer active:scale-95 ">
-                            register
+                            Register
                         </a>
                         <a
                             href="/login"
                             className="text-(--bg-color)/75 hover:text-(--bg-color) w-full text-left py-2 md:text-center rounded-lg transition-colors cursor-pointer active:scale-95">
-                            login
+                            Login
                         </a>
 
                     </div>
 
                 </div>
                 {/* typography */}
-                <div className="text-(--bg-color)/75 font-black text-7xl flex items-end">
+                <div className="text-(--accent-blue) font-black text-5xl flex items-end uppercase">
                     <a href="/">Egg</a>
                 </div>
             </div>
